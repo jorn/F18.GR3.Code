e014159b06f7e5d8b9a45f404b2597b7b9add1cb
Unknown <Noirpale@gmail.com>
2018-04-24 10:10:50 +0200

    Buffer.c ready

--- buffer.c
+++ buffer.c
@@ -1,91 +1,102 @@
 /*****************************************************************************
- * University of Southern Denmark
- *
- * MODULENAME.: audio.h
- *
- * PROJECT....: F18.GR3.Code
- *
- * DESCRIPTION:
- *
- * Change Log:
- ******************************************************************************
- * Date    Id    Change
- * YYMMDD
- * --------------------
- * Apr 19, 2018	jorn    Module created.
- *
- *****************************************************************************/
+* University of Southern Denmark
+* Embedded Programming (EMP)
+*
+* MODULENAME.: buffer.c
+*
+* PROJECT....: F18.GR3.Code
+*
+* DESCRIPTION: See module specification file (.h-file).
+*
+* Change Log:
+******************************************************************************
+* Date			Id	  Change
+* MMM DD, YYYY
+* ----------------------------------------------------------------------------
+* Apr 24, 2018	SoF   Module created.
+*
+*****************************************************************************/
 
-#ifndef BUFFER_H_
-#define BUFFER_H_
+/***************************** Include files ********************************/
+#include "buffer.h"
+/*****************************    Defines    ********************************/
 
-/***************************** Include files *******************************/
-#include "emp_type.h"
-#include <stdint.h>
-/*****************************    Defines    *******************************/
+/*****************************   Constants   ********************************/
 
+/*****************************   Variables   ********************************/
 
-/********************** External declaration of Variables ******************/
-typedef struct {
-  float * buffer;
-  size_t tail; //buffer for audio samples
-  size_t head; //pointer to head in buffer
-  size_t size; 
-} sample_buffer_t;
+/*****************************   Functions   *********************************
+*   Function : See General module specification (general.h-file).
+*****************************************************************************/
 
-static sample_buffer_t sample_buffer;
-/*****************************   Constants   *******************************/
-
-/*************************  Function interfaces ****************************/
-
-int8_t sample_buffer_reset(sample_buffer_t * sample_buffer);
+int8_t sample_buffer_reset(sample_buffer_t * sample_buffer)
 /*****************************************************************************
  *    Input    : The buffer to be reset
- *    Output   : 
+ *    Output   :
  *    Function : Resets the buffer.
  *******************************************************************************/
+{
 
-int8_t sample_buffer_put(sample_buffer_t * sample_buffer, float data);
-/*****************************************************************************
- *    Input    : The buffer data should be put into and the data to put in.
- *    Output   : 
- *    Function : Inserts data into the buffer.
- *******************************************************************************/
+}
 
-int8_t sample_buffer_get(sample_buffer_t * sample_buffer, float * data);
-/*****************************************************************************
- *    Input    : The buffer data is to be got from.
- *    Output   : 
- *    Function : Gets data from buffer.
- *******************************************************************************/
-
-int8_t sample_buffer_put_z(sample_buffer_t * sample_buffer, float data);
+int8_t sample_buffer_put(sample_buffer_t * sample_buffer, float data)
 /*****************************************************************************
  *    Input    : The buffer data should be put into and the data to put in.
  *    Output   :
  *    Function : Inserts data into the buffer.
  *******************************************************************************/
+{
 
-int8_t sample_buffer_get_z(sample_buffer_t * sample_buffer, float * data);
+}
+
+int8_t sample_buffer_get(sample_buffer_t * sample_buffer, float * data)
 /*****************************************************************************
  *    Input    : The buffer data is to be got from.
  *    Output   :
  *    Function : Gets data from buffer.
  *******************************************************************************/
+{
 
-bool sample_buffer_empty(sample_buffer_t sample_buffer);
+}
+
+int8_t sample_buffer_put_z(sample_buffer_t * sample_buffer, float data, int z)
+/*****************************************************************************
+ *    Input    : The buffer data should be put into and the data to put in.
+ *    Output   :
+ *    Function : Inserts data into the buffer at location offset from header by z.
+ *******************************************************************************/
+{
+
+}
+
+int8_t sample_buffer_get_z(sample_buffer_t * sample_buffer, float * data, int z)
+/*****************************************************************************
+ *    Input    : The buffer data is to be got from.
+ *    Output   :
+ *    Function : Gets data from buffer at location offset from header by z.
+ *******************************************************************************/
+{
+
+}
+
+bool sample_buffer_empty(sample_buffer_t sample_buffer)
 /*****************************************************************************
  *    Input    : The buffer to check.
  *    Output   : Outputs 1 if buffer is empty and 0 if not.
  *    Function : Checks if a buffer is empty.
  *******************************************************************************/
+{
 
-bool sample_buffer_full(sample_buffer_t sample_buffer);
+}
+
+bool sample_buffer_full(sample_buffer_t sample_buffer)
 /*****************************************************************************
  *    Input    : The buffer to check.
  *    Output   : Outputs 1 if buffer is full and 0 if not.
  *    Function : Checks if a buffer is full.
  *******************************************************************************/
+{
+
+}
 
 /****************************** End Of Module *******************************/
-#endif /* BUFFER_H_ */
