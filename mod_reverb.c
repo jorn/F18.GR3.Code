462b1079441e8f4ab6a4503d5c223b8708b187c8
Unknown <Noirpale@gmail.com>
2018-04-25 09:47:13 +0200

    Reverb added

---
+++ mod_reverb.c
@@ -0,0 +1,68 @@
+/*****************************************************************************
+ * University of Southern Denmark
+ *
+ * MODULENAME.: mod_reverb.c
+ *
+ * PROJECT....: F18.GR3
+ *
+ * DESCRIPTION: 
+ *
+ * Change Log:
+ *****************************************************************************
+ * Date    Id    Change
+ * --------------------
+ * Apr 25, 2018  SoF    Module created.
+ *
+ *****************************************************************************/
+
+/***************************** Include files *******************************/
+#include <stdint.h>
+#include "audio.h"
+#include "buffer.h"
+#include "hardware.h"
+
+/*****************************    Defines    *******************************/
+
+/*****************************   Constants   *******************************/
+
+/*****************************   Variables   *******************************/
+
+/*****************************   Functions   *******************************/
+void mod_reverb_effekt( fp_sample_t *in, fp_sample_t *out)
+{
+  if(is_sw1_pressed())
+  {
+  const float in_gain = -0.25;
+  const float re_gain = -0.05;
+  const uint16_t delay = 1500;
+
+  fp_sample_t fp_sample;
+  sample_buffer_get_z( &fp_sample, delay);
+
+  out->left_fp32 = in->left_fp32 + (fp_sample.left_fp32 * in_gain);
+  out->right_fp32 = in->right_fp32 + (fp_sample.right_fp32 * in_gain);
+
+  fp_sample.left_fp32 = (out->left_fp32 * re_gain);
+  fp_sample.right_fp32 = (out->right_fp32 * re_gain);
+  sample_buffer_put_z(&fp_sample, 1);
+
+  }
+  else
+  {
+    out->left_fp32 = in->left_fp32;
+    out->right_fp32 = in->right_fp32;
+  }
+}
+
+void mod_reverb_init( void )
+{
+
+}
+
+void mod_reverb_ui( void )
+{
+
+}
+
+
+/****************************** End Of Module *******************************/
