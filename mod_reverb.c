5753a22c82f95b7ab8ba4ebeed523b4bd30f39da
Unknown <Noirpale@gmail.com>
2018-04-25 12:12:27 +0200

    Reverb Test

--- mod_reverb.c
+++ mod_reverb.c
@@ -33,17 +33,17 @@ void mod_reverb_effekt( fp_sample_t *in, fp_sample_t *out)
   if(is_sw1_pressed())
   {
   const float in_gain = -0.25;
-  const float re_gain = -0.05;
+  const float fb_gain = -0.05;
   const uint16_t delay = 1500;
 
   fp_sample_t fp_sample;
   sample_buffer_get_z( &fp_sample, delay);
 
-  out->left_fp32 = in->left_fp32 + (fp_sample.left_fp32 * in_gain);
-  out->right_fp32 = in->right_fp32 + (fp_sample.right_fp32 * in_gain);
+  out->left_fp32 = in->left_fp32 + fp_sample.left_fp32;
+  out->right_fp32 = in->right_fp32 + fp_sample.right_fp32;
 
-  fp_sample.left_fp32 = (out->left_fp32 * re_gain);
-  fp_sample.right_fp32 = (out->right_fp32 * re_gain);
+  fp_sample.left_fp32 = ((out->left_fp32 * fb_gain) + (in->left_fp32 * in_gain));
+  fp_sample.right_fp32 = ((out->right_fp32 * fb_gain) + (in->right_fp32 * in_gain));
   sample_buffer_put_z(&fp_sample, 1);
 
   }
